90a4a6aae0fd017200f42a8e9d753fcadf76ac28
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-05-11 19:57:54 +0100

    added MazeGenerator

--- Assets/MazeCell.cs
+++ Assets/MazeCell.cs
@@ -10,7 +10,9 @@ public class MazeCell : MonoBehaviour
     [SerializeField] bool southWallActive = true;
     [SerializeField] bool westWallActive = true;
 
-    public GameObject northWall, eastWall, southWall, westWall;
+    internal const int cellSize = 1;
+
+    GameObject northWall, eastWall, southWall, westWall;
 
     // Use this for initialization
     void Start()
---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField] Vector2Int size;
+    [SerializeField] MazeCell cellPrefab;
+
+    private MazeCell[,] cells;
+
+    // Use this for initialization
+    void Start()
+	{
+        InitializeMaze();
+	}
+
+    private void InitializeMaze()
+    {
+        cells = new MazeCell[size.x, size.y];
+
+        for (int r = 0; r < size.x; r++)
+        {
+            for (int c = 0; c < size.y; c++)
+            {
+                CreateCell(r, c);
+            }
+        }
+    }
+
+    private void CreateCell(int r, int c)
+    {
+        // Instantiate Cell
+        MazeCell newCell = Instantiate(cellPrefab, transform) as MazeCell;
+        newCell.name = r + "," + c;
+        newCell.transform.position = new Vector3(r * MazeCell.cellSize, 0f, c * MazeCell.cellSize);
+
+        // Add to the maze
+        cells[r, c] = newCell;
+        return;
+    }
+}
