bd89fd14152e41eb7637232f5962c4076c45eafb
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-05-14 19:04:35 +0100

    added pickups
    -added finish prefab for the end game

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int counter;
+
+	// Use this for initialization
+	void Start()
+    {
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            counter++;
+        }
+        else if (other.gameObject.CompareTag("Finish"))
+        {
+            if (counter == 3)
+            {
+                print("Game Over");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    const float tau = 2f * Mathf.PI;
+
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPosition;
+
+    // Use this for initialization
+    void Start()
+	{
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+        if (period <= Mathf.Epsilon) { return; } // protect from period = 0
+
+        float cycles = Time.time / period; // grows continually from 0
+        movementFactor = Mathf.Sin(cycles * tau) / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+	}
+}
