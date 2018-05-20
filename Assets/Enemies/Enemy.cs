549903d8660f9e7abeb1184aa740566bc6b7a12e
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-05-20 11:58:44 +0100

    added pathfinding to the enemies

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour
 {
@@ -13,17 +14,30 @@ public class Enemy : MonoBehaviour
     }
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 5f;
 
     float currentHealtPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
+
     // Use this for initialization
     void Start()
     {
-		
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
-		
+        float distanceToPlayer = Vector3.Distance(player.transform.position,transform.position);
+		if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
 	}
 }
