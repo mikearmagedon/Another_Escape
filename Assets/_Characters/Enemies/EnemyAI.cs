f5a77486945cdf2ff0175555a28218f481c008f9
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-05-27 16:43:46 +0100

    added ChasePlayer coroutine to EnemyAI

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Assertions;
 
 namespace RPG.Characters
 {
+    // EnemyAI states
+    enum State { idle, patrolling, chasing, attacking};
+
     [SelectionBase]
     [RequireComponent(typeof(Character))]
     [RequireComponent(typeof(WeaponSystem))]
@@ -14,6 +18,8 @@ namespace RPG.Characters
         [SerializeField] float chaseRadius = 10f;
 
         // State
+        State state = State.idle;
+        float distanceToPlayer;
         float currentWeaponRange;
 
         // Cached components references
@@ -33,14 +39,43 @@ namespace RPG.Characters
             WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
             currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
 
-            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+            if (distanceToPlayer > chaseRadius && state != State.patrolling)
+            {
+                StopAllCoroutines();
+                state = State.patrolling;
+            }
+
+            if (distanceToPlayer <= chaseRadius && state != State.chasing)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ChasePlayer());
+            }
+
+            if (distanceToPlayer <= currentWeaponRange && state != State.attacking)
+            {
+                StopAllCoroutines();
+                state = State.attacking;
+            }
         }        
+
+        IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            while (distanceToPlayer > currentWeaponRange)
+            {
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
+        }
 
         void OnDrawGizmos()
         {
             // Draw attack radius sphere
             Gizmos.color = new Color(255f, 0f, 0f, 0.5f);
-            Gizmos.DrawWireSphere(transform.position, currentWeaponConfig.GetMaxAttackRange());
+            Gizmos.DrawWireSphere(transform.position, currentWeaponRange);
 
             // Draw chase radius sphere
             Gizmos.color = new Color(0f, 0f, 255f, 0.5f);
