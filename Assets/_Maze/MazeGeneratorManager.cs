20adfb5a4d58e77b3521589067226fb266882892
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-06-04 14:22:48 +0100

    added algorithm selection to MazeGenerator prefab

--- Assets/_Maze/MazeGeneratorManager.cs
+++ Assets/_Maze/MazeGeneratorManager.cs
@@ -9,6 +9,7 @@ public class MazeGeneratorManager
     [HideInInspector] public GameObject mazeGeneratorInstance;
     [SerializeField] Vector2Int initialCoordinates = new Vector2Int(0, 0);
     [SerializeField] Vector2Int size = new Vector2Int(10, 10);
+    [SerializeField] Algorithm mazeAlgorithm = Algorithm.BinaryTree;
     [SerializeField] float secondsBetweenGenerations = 20f;
     [SerializeField] Vector2Int mazeEntrance = new Vector2Int(0, 0);
     [SerializeField] MazeDirection mazeEntranceDirection = MazeDirection.None;
@@ -23,6 +24,7 @@ public class MazeGeneratorManager
 
         mazeGenerator.size = size;
         mazeGenerator.initialCoordinates = initialCoordinates;
+        mazeGenerator.mazeAlgorithm = mazeAlgorithm;
         mazeGenerator.secondsBetweenGenerations = secondsBetweenGenerations;
         mazeGenerator.mazeEntrance = mazeEntrance;
         mazeGenerator.mazeEntranceDirection = mazeEntranceDirection;
