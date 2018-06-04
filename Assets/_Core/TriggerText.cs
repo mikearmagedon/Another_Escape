71ddfcc082610b7678905db3b3f95e9270b9aa86
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-06-04 16:59:17 +0100

    created TriggerText prefab

---
+++ Assets/_Core/TriggerText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerText : MonoBehaviour
+{
+    [SerializeField] Text textBox;
+    [SerializeField] string text;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!textBox.enabled)
+            {
+                textBox.enabled = true;
+            }
+
+            textBox.text = text;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (textBox.enabled)
+            {
+                textBox.enabled = false;
+            }
+
+            textBox.text = string.Empty;
+        }
+    }
+}
