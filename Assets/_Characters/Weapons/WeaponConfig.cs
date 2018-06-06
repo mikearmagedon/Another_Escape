ca89c4ca9c76c7c1487d32d937298ac97af7e9de
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-06-06 18:09:32 +0100

    missing unstaged files

--- Assets/_Characters/Weapons/WeaponConfig.cs
+++ Assets/_Characters/Weapons/WeaponConfig.cs
@@ -17,7 +17,6 @@ namespace RPG.Characters
         [SerializeField] float maxAttackRange = 2f;
         [SerializeField] float weaponDamage = 1f;
         [SerializeField] float damageDelay = 0.5f;
-		[SerializeField] AudioClip attackSFX;
 
         public float GetWeaponDamage()
         {
