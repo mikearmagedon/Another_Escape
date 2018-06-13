aa8cfbeed20809fff014ac63c165abafe5d3135f
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-06-13 16:23:59 +0100

    added Main Menu

---
+++ Assets/_CameraUI/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
