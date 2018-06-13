3dfbb2da5b662ce0c0489d8869efe9d56d61a116
Miguel Araújo <miguel.alex.macedo@gmail.com>
2018-06-13 19:37:24 +0100

    updated TODO

--- Assets/_CameraUI/Menu.cs
+++ Assets/_CameraUI/Menu.cs
@@ -17,6 +17,10 @@ public class Menu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
     }
 }
