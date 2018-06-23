0f55c1671b33dee1c3a8f9c0d0ba351d148ff30c
Ricardo Sousa <ricardo.sousa1993@hotmail.com>
2018-06-23 14:49:42 +0100

    added trap and dust_fx
    added trap to maze 3
    added dust particles to trap

---
+++ Assets/_Environment/Traps/TrapOpen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapOpen : MonoBehaviour {
+
+    public GameObject TrapDoor;
+    public GameObject FX_Dust;
+
+    private Animation anim;
+
+    void OnTriggerEnter()
+    {
+        anim = TrapDoor.GetComponent<Animation>();
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 2F;
+        }
+        anim.Play("TrapAnimRock");
+        TrapDoor.GetComponent<Collider>().enabled = false;
+        TrapDoor.GetComponent<MeshRenderer>().enabled = false;
+        this.gameObject.GetComponent<Collider>().enabled = false;
+        FX_Dust.SetActive(true);
+    }
+
+}
