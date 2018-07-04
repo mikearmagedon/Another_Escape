d9d1c3c5f4cf5ffc412537828dd870cbebfac200
Ricardo Sousa <ricardo.sousa1993@hotmail.com>
2018-07-04 19:28:30 +0100

    added new musics
    added ambient sounds to diferent zones

---
+++ Assets/_Levels/World 1/background_sound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_sound : MonoBehaviour
+{
+
+    public AudioSource music;
+
+    public void ChangeMusic(AudioClip clip)
+    {
+        if (music.clip.name == clip.name)
+        {
+            return;
+        }
+
+        music.clip = clip;
+        StartCoroutine(FadeIn());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        float t = 0.5f;
+        while (t >= 0)
+        {
+            t -= Time.deltaTime;
+            music.volume = t;
+            yield return new WaitForSeconds(0);
+        }
+        music.volume = 0.0f;
+        music.Stop();
+        music.loop = false;
+    }
+
+    IEnumerator FadeIn()
+    {
+        music.volume = 0.0f;
+        music.Play();
+        music.loop = false;
+
+        float t = 0.0f;
+        while (music.volume <= 0.5)
+        {
+            t += Time.deltaTime;
+            music.volume = t;
+            yield return new WaitForSeconds(0);
+        }
+
+    }
+}
