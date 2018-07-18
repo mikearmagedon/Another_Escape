8b5660f6b3d04a13888e857e60a336f22c2bb5cf
Ricardo Sousa <ricardo.sousa1993@hotmail.com>
2018-07-18 19:09:16 +0100

    added npcs to mainhub
    added sound to Maze_Trap
    see TODO for INFO

--- Assets/_Environment/Traps/Maze_Trap.cs
+++ Assets/_Environment/Traps/Maze_Trap.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Maze_Trap : MonoBehaviour {
 
+    public AudioClip clip;
+    AudioManager audioManager;
+
 	// Use this for initialization
 	void Start () {
+        audioManager = AudioManager.instance;
         InvokeRepeating("PlayAnimation", 0f,2f);
 	}
 
@@ -13,5 +17,6 @@ public class Maze_Trap : MonoBehaviour {
     {
         GetComponent<Animation>()["Maze_Trap"].speed = 2f;
         GetComponent<Animation>().Play();
+        audioManager.PlayMisc(clip);
     }
 }
